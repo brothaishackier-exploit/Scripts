61df150902179b2deb4e3c1616d2708d87b819ab
brothaishackier-exploit <brothaishackier@gmail.com>
2025-12-23 21:11:51 +0100

    Update BROTHAISHACKIER HUB.lua

--- BROTHAISHACKIER HUB.lua
+++ BROTHAISHACKIER HUB.lua
@@ -1,10 +1,13 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+-- Load Rayfield
+local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+
+-- Create Window
 local Window = Rayfield:CreateWindow({
    Name = "BROTHAISHACKIER HUB",
-   Icon = 81944506050305, 
+   Icon = 81944506050305,
    LoadingTitle = "BROTHAISHACKIER HUB",
    LoadingSubtitle = "by BROTHAISHACKIER",
-   ShowText = "BROTHAISHACKIER HUB", 
+   ShowText = "BROTHAISHACKIER HUB",
    Theme = "DarkBlue",
 
    ToggleUIKeybind = "K",
@@ -14,14 +17,14 @@ local Window = Rayfield:CreateWindow({
 
    ConfigurationSaving = {
       Enabled = false,
-      FolderName = nil, 
+      FolderName = nil,
       FileName = "Big Hub"
    },
 
    Discord = {
       Enabled = true,
-      Invite = "https://discord.gg/Kqh2QMBsBP",
-      RememberJoins = false 
+      Invite = "Kqh2QMBsBP",
+      RememberJoins = false
    },
 
    KeySystem = false,
@@ -32,60 +35,68 @@ local Window = Rayfield:CreateWindow({
       FileName = "Key",
       SaveKey = true,
       GrabKeyFromSite = false,
-      Key = {"Hello"}
+      Key = { "Hello" }
    }
 })
 
-local Tab = Window:CreateTab("Universal", "globe")
+-- Universal Tab
+local UniversalTab = Window:CreateTab("Universal", "globe")
 
-local Button = Tab:CreateButton({
+UniversalTab:CreateButton({
    Name = "Super Executor",
    Callback = function()
-   loadstring(game:HttpGet('https://raw.githubusercontent.com/brothaishackier-exploit/Scripts/refs/heads/main/Super-Executor.lua'))()
-   end,
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/brothaishackier-exploit/Scripts/refs/heads/main/Super-Executor.lua"))()
+   end
 })
 
-local Tab = Window:CreateTab("Specific", "goal")
+-- Specific Tab
+local SpecificTab = Window:CreateTab("Specific", "goal")
 
-local Button = Tab:CreateButton({
+SpecificTab:CreateButton({
    Name = "FE Flex your fps",
    Callback = function()
-   loadstring(game:HttpGet('https://raw.githubusercontent.com/brothaishackier-exploit/Scripts/refs/heads/main/FE%20flex%20your%20fps.lua'))()
-   end,
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/brothaishackier-exploit/Scripts/refs/heads/main/FE%20flex%20your%20fps.lua"))()
+   end
 })
 
-local Tab = Window:CreateTab("About BROTHAISHACKIER", 81944506050305)
-local Section = Tab:CreateSection("About")
-local Label = Tab:CreateLabel("BROTHAISHACKIER", 85624417780432)
-local paragraph2 = Tab:CreateParagraph({
-    Title = "About",
-    Content = [[
+-- About Tab
+local AboutTab = Window:CreateTab("About BROTHAISHACKIER", 81944506050305)
+
+AboutTab:CreateSection("About")
+
+AboutTab:CreateLabel("BROTHAISHACKIER", 85624417780432)
+
+AboutTab:CreateParagraph({
+   Title = "About",
+   Content = [[
 I make roblox hacks
-    ]]
+   ]]
 })
 
-local Section = Tab:CreateSection("Links")
+AboutTab:CreateSection("Links")
 
-local Button = Tab:CreateButton({
+AboutTab:CreateButton({
    Name = "ScriptBlox",
    Callback = function()
-   setclipboard("https://scriptblox.com/u/BROTHAISHACKIER")
-   Rayfield:Notify({
-      Title = "Copied to Clipboard",
-      Content = "Paste it in a Browser",
-      Duration = 6.5,
-      Image = "map-pin",
-   })
-   end,
-local Button = Tab:CreateButton({
+      setclipboard("https://scriptblox.com/u/BROTHAISHACKIER")
+      Rayfield:Notify({
+         Title = "Copied to Clipboard",
+         Content = "Paste it in a Browser",
+         Duration = 6.5,
+         Image = "map-pin"
+      })
+   end
+})
+
+AboutTab:CreateButton({
    Name = "HaxHell",
    Callback = function()
-   setclipboard("https://haxhell.com/user/brothaishackier")
-   Rayfield:Notify({
-      Title = "Copied to Clipboard",
-      Content = "Paste it in a Browser",
-      Duration = 6.5,
-      Image = "map-pin",
-   })
-   end,
+      setclipboard("https://haxhell.com/user/brothaishackier")
+      Rayfield:Notify({
+         Title = "Copied to Clipboard",
+         Content = "Paste it in a Browser",
+         Duration = 6.5,
+         Image = "map-pin"
+      })
+   end
 })
